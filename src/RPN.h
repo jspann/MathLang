7817ef0bff6a23d86201b424fae431b359316542
James Spann <thedeveloper4@gmail.com>
2014-10-23 19:45:55 -0400

    Working on math parsing

---
+++ src/RPN.h
@@ -0,0 +1,16 @@
+//
+//  RPN.h
+//  
+//
+//  Created by James Spann on 10/23/14.
+//
+//
+
+#ifndef ____RPN__
+#define ____RPN__
+
+#include <iostream>
+using namespace std;
+int parse(const char *s);
+
+#endif /* defined(____RPN__) */
